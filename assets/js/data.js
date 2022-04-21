a811ec745f0ff315c8c17eac789770289363519e
Michiel Stragier <Michiel.stragier@gmail.com>
2022-04-21 12:35:18 +0200

    file for global variables

---
+++ assets/js/data.js
@@ -0,0 +1,2 @@
+const _local = {tel: '0497659529', email: 'michiel.stragier@gmail.com'}
+const _pricing = {smallCar : '€500', mediumCar: '€750', bigCar: "€1 000"} //These prices aren't final yea know
