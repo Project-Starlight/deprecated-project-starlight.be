eb61e3e2557797f678a49bec53cc4f903b613358
Michiel Stragier <Michiel.stragier@gmail.com>
2022-05-24 21:19:36 +0200

    made mutliple initializers

--- assets/js/initialiser.js
+++ assets/js/initialiser.js
@@ -4,61 +4,93 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     if (document.querySelector("header")) {
-        document.querySelector("header #icons").addEventListener('click', header_icon);
-        document.querySelector("header h1").addEventListener('click', () => {
-            navigateToDifferentHtmlPage("../index.html")
-        });
+        headerInitializer();
     }
     if (document.querySelector("#prijs-pagina")) {
-        renderPrice();
-        document.querySelectorAll("#prijs-pagina main div div").forEach((Element) => {
-            Element.addEventListener('click', navigateFromPricingToContact)
-        });
+        prijsPaginaInitializer();
     }
     if (document.querySelector("#contactUs")) {
-        if (loadFromStorage("selectedPriceCategory")) {
-            document.querySelectorAll("#pricingOption option").forEach((Element) => {
-                if (loadFromStorage("selectedPriceCategory") === Element.value) {
-                    Element.selected = "selected";
-                }
-            });
-        }
-        document.querySelector(".begin button").addEventListener("click", (e) => {
-            navigateWithHidden(".begin", ".algemene-informatie", e)
-        });
-        document.querySelector(".algemene-informatie button[name='terug']").addEventListener("click", (e) => {
-            navigateWithHidden(".algemene-informatie", ".begin", e)
-        });
-        document.querySelector(".algemene-informatie button[name='volgende']").addEventListener("click", (e) => {
-            navigateWithHidden(".algemene-informatie", ".product-informatie", e)
-        });
-        document.querySelector(".product-informatie button[name='terug']").addEventListener("click", (e) => {
-            navigateWithHidden(".product-informatie", ".algemene-informatie", e)
-        });
-        document.querySelector(".product-informatie button[name='volgende']").addEventListener("click", (e) => {
-            navigateWithHidden(".product-informatie", ".bericht", e)
-        });
-        document.querySelector(".bericht button[name='terug']").addEventListener("click", (e) => {
-            navigateWithHidden(".bericht", ".product-informatie", e)
-        });
-        document.querySelector(".bericht button[name='volgende']").addEventListener("click", (e) => {
-            navigateWithHidden(".bericht", ".final-check", e);
-            showFinalCheck(e);
-        });
-        document.querySelector(".final-check button[name='terug']").addEventListener("click", (e) => {
-            navigateWithHidden(".final-check", ".bericht", e)
-        });
-        document.querySelector(".final-check input[name='verstuur']").addEventListener("click", (e) => {
-            if (!checkRequirements(e)) {
-                postContactForm(e);
-            }
-        });
-        document.querySelector(".form-error ul").addEventListener("click", (e) => {
-            navigateWithHidden(".form-error", ".algemene-informatie", e);
-            document.querySelector(`.algemene-informatie input[id="${e.target.closest("li").className}"]`).focus();
-        });
-
+        contactUsPageInitializer()
     }
 
+}
+
+function headerInitializer(){
+    document.querySelector("header #icons").addEventListener('click', header_icon);
+    document.querySelector("header h1").addEventListener('click', () => {
+        navigateToDifferentHtmlPage("../index.html")
+    });
+}
+
+function prijsPaginaInitializer(){
+    renderPrice();
+    document.querySelectorAll("#prijs-pagina main div div").forEach((Element) => {
+        Element.addEventListener('click', navigateFromPricingToContact)
+    });
+}
+
+function contactUsPageInitializer(){
+    if (loadFromStorage("selectedPriceCategory")) {
+        document.querySelectorAll("#pricingOption option").forEach((Element) => {
+            if (loadFromStorage("selectedPriceCategory") === Element.value) {
+                Element.selected = "selected";
+            }
+        });
+    }
+    document.querySelector(".begin button").addEventListener("click", (e) => {
+        navigateWithHidden(".begin", ".algemene-informatie", e)
+    });
+
+    /* Algemene informatie navigatie */
+
+    document.querySelector(".algemene-informatie button[name='terug']").addEventListener("click", (e) => {
+        navigateWithHidden(".algemene-informatie", ".begin", e)
+    });
+    document.querySelector(".algemene-informatie button[name='volgende']").addEventListener("click", (e) => {
+        navigateWithHidden(".algemene-informatie", ".product-informatie", e)
+    });
+
+    /* Product informatie navigatie */
+
+    document.querySelector(".product-informatie button[name='terug']").addEventListener("click", (e) => {
+        navigateWithHidden(".product-informatie", ".algemene-informatie", e)
+    });
+    document.querySelector(".product-informatie button[name='volgende']").addEventListener("click", (e) => {
+        navigateWithHidden(".product-informatie", ".bericht", e)
+    });
+
+    /* Bericht navigatie */
+
+    document.querySelector(".bericht button[name='terug']").addEventListener("click", (e) => {
+        navigateWithHidden(".bericht", ".product-informatie", e)
+    });
+    document.querySelector(".bericht button[name='volgende']").addEventListener("click", (e) => {
+        navigateWithHidden(".bericht", ".final-check", e);
+        showFinalCheck(e);
+    });
+
+    /* Show final check */
+
+    document.querySelector(".final-check button[name='terug']").addEventListener("click", (e) => {
+        navigateWithHidden(".final-check", ".bericht", e)
+    });
+    document.querySelector(".final-check input[name='verstuur']").addEventListener("click", (e) => {
+        if (!checkRequirements(e)) {
+            postContactForm(e);
+        }
+    });
+
+    /**/
+
+    document.querySelector(".form-error ul").addEventListener("click", (e) => {
+        navigateWithHidden(".form-error", ".algemene-informatie", e);
+        document.querySelector(`.algemene-informatie input[id="${e.target.closest("li").className}"]`).focus();
+    });
+
+    /**/
+
+    document.querySelector(".form-succes button").addEventListener("click",(e)=>{
+        navigateToDifferentHtmlPage("../index.html");
+    });
 }
 
--- assets/js/render/render-form.js
+++ assets/js/render/render-form.js
@@ -1,22 +1,38 @@
 "use strict";
-function showFinalCheck(e){
-    const html =
-        `<p>${document.querySelector(".algemene-informatie input[name='voornaam']").value}</p>
-                    <p>${document.querySelector(".algemene-informatie input[name='achternaam']").value}</p>
-                    <p>${document.querySelector(".algemene-informatie input[name='email']").value}</p>
-                    <p>${document.querySelector(".algemene-informatie input[name='telefoon']").value}</p>
-                    <p>${document.querySelector(".product-informatie input[name='merk']").value}</p>
-                    <p>${document.querySelector(".product-informatie input[name='model']").value}</p>
-                    <p>${document.querySelector(".product-informatie input[name='jaar']").value}</p>
-                    <p>${refactorSelect(document.querySelector(".product-informatie select").value)}</p>
-                    <p>${document.querySelector(".bericht textarea").value}</p>`
-    document.querySelector(".final-check div div:last-child").innerHTML = html;
+
+function showFinalCheck(e) {
+    const selectors = {
+        Voornaam: ".algemene-informatie input[name='voornaam']",
+        Achternaam: ".algemene-informatie input[name='achternaam']",
+        Email: ".algemene-informatie input[name='email']",
+        Telefoon: ".algemene-informatie input[name='telefoon']",
+        Merk: ".product-informatie input[name='merk']",
+        Model: ".product-informatie input[name='model']",
+        Optie: ".product-informatie select",
+        Jaar: ".product-informatie input[name='jaar']",
+        Bericht: ".bericht textarea"
+    }
+    let htmlTitle = ``;
+    let htmlTxt = ``;
+    for (let selectorsKey in selectors) {
+        if (getValue(selectors[selectorsKey]) !== "kies-optie" && getValue(selectors[selectorsKey])){
+            htmlTitle += `<p>${selectorsKey}</p>`;
+            htmlTxt += `<p class="${selectorsKey}">${getValue(selectors[selectorsKey])}</p>`;
+        }
+    }
+
+    document.querySelector(".final-check div div:first-child").innerHTML = htmlTitle;
+    document.querySelector(".final-check div div:last-child").innerHTML = htmlTxt;
 }
 
-function showFormError(requiredFormFieldsThatArentFilledIn,e) {
+function showFormError(requiredFormFieldsThatArentFilledIn, e) {
     document.querySelector(".form-error ul").innerHTML = "";
     requiredFormFieldsThatArentFilledIn.forEach(function (field) {
         document.querySelector(".form-error ul").insertAdjacentHTML("beforeend", `<li class="${field}"><p>${field}</p><i class="fa-solid fa-pencil"></i></li>`);
     })
-    navigateWithHidden(".final-check",".form-error",e);
+    navigateWithHidden(".final-check", ".form-error", e);
+}
+
+function getValue(selector) {
+    return document.querySelector(`${selector}`).value;
 }
